qt/qtdeclarative

Replace QQuickColorImageProvider with QQuickColorImage
Now that QQuickImage is exported, we can colorize images using a
simple QQuickImage subclass instead of using an image provider. The
main issue with QQuickColorImageProvider was that it was based on
QGuiApplication::devicePixelRatio(). Now each QQuickColorImage handles
its device pixel ratio correctly depending on which screen it is on.

Even though we now have to use two bindings (color and source) instead
of encoding the color to the source, at the same time we can remove the
sourceSize bindings that were added as image provider specific high DPI
workarounds (ca87ab8).

Task-number: QTBUG-58925
Change-Id: Iba14d2afe3bda540189682ba5be7c58d907d88f7
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/imports/controls/universal/SpinBox.qml
@@ -36,6 +36,8 @@
 
 import QtQuick 2.9
 import QtQuick.Templates 2.3 as T
+import QtQuick.Controls 2.3
+import QtQuick.Controls.impl 2.3
 import QtQuick.Controls.Universal 2.3
 
 T.SpinBox {
@@ -99,14 +101,12 @@ T.SpinBox {
             opacity: control.activeFocus && !control.up.pressed ? 0.4 : 1.0
         }
 
-        Image {
+        ColorImage {
             x: (parent.width - width) / 2
             y: (parent.height - height) / 2
-            source: "image://universal/" + (control.mirrored ? "left" : "right") + "arrow/"
-                    + (!enabled ? control.Universal.chromeDisabledLowColor :
-                                  control.activeFocus ? control.Universal.chromeBlackHighColor : control.Universal.baseHighColor)
-            sourceSize.width: width
-            sourceSize.height: height
+            color: !enabled ? control.Universal.chromeDisabledLowColor :
+                              control.activeFocus ? control.Universal.chromeBlackHighColor : control.Universal.baseHighColor
+            source: "qrc:/qt-project.org/imports/QtQuick/Controls.2/Universal/images/" + (control.mirrored ? "left" : "right") + "arrow.png"
         }
     }
 
@@ -127,14 +127,12 @@ T.SpinBox {
             opacity: control.activeFocus && !control.down.pressed ? 0.4 : 1.0
         }
 
-        Image {
+        ColorImage {
             x: (parent.width - width) / 2
             y: (parent.height - height) / 2
-            source: "image://universal/" + (control.mirrored ? "right" : "left") + "arrow/"
-                    + (!enabled ? control.Universal.chromeDisabledLowColor :
-                                  control.activeFocus ? control.Universal.chromeBlackHighColor : control.Universal.baseHighColor)
-            sourceSize.width: width
-            sourceSize.height: height
+            color: !enabled ? control.Universal.chromeDisabledLowColor :
+                              control.activeFocus ? control.Universal.chromeBlackHighColor : control.Universal.baseHighColor
+            source: "qrc:/qt-project.org/imports/QtQuick/Controls.2/Universal/images/" + (control.mirrored ? "right" : "left") + "arrow.png"
         }
     }
 

ubports/qqc2-suru-style

qqc2-suru/SpinBox.qml
@@ -22,6 +22,8 @@
 ****************************************************************************/
 
 import QtQuick 2.9
+import QtQuick.Controls 2.3
+import QtQuick.Controls.impl 2.3
 import QtQuick.Templates 2.2 as T
 import QtQuick.Controls.Suru 2.2
 
@@ -87,11 +89,11 @@ T.SpinBox {
             }
         }
 
-        Image {
+        ColorImage {
             anchors.centerIn: parent
             width: 16; height: width
-            source: "image://suru/" + (control.mirrored ? "left" : "right") + "arrow/"
-                    + control.Suru.foregroundColor
+            color: control.Suru.foregroundColor
+            source: "qrc:/qt-project.org/imports/QtQuick/Controls.2/Suru/assets/" + (control.mirrored ? "left" : "right") + "arrow.png"
             sourceSize.width: width
             sourceSize.height: height
         }
@@ -120,11 +122,11 @@ T.SpinBox {
             }
         }
 
-        Image {
+        ColorImage {
             anchors.centerIn: parent
             width: control.Suru.units.gu(2); height: width
-            source: "image://suru/" + (control.mirrored ? "right" : "left") + "arrow/"
-                    + control.Suru.foregroundColor
+            color: control.Suru.foregroundColor
+            source: "qrc:/qt-project.org/imports/QtQuick/Controls.2/Suru/assets/" + (control.mirrored ? "right" : "left") + "arrow.png"
             sourceSize.width: width
             sourceSize.height: height
         }